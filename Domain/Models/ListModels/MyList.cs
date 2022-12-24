70c81db1b7b83a2c44cc967c9a499455f9db0221
kamranss <kamranss@code.edu.az>
2022-12-24 14:53:39 +0400

    Feature: List Methods added

--- Domain/Models/ListModels/MyList.cs
+++ Domain/Models/ListModels/MyList.cs
@@ -1,20 +1,34 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Models.ListModels
 {
-    public class MyList<T>
+    public class MyList<T>:IEnumerable
     {
-       public static  T[] arr;
+       public   T[] arr;
 
-       public MyList()
+        public int Count { get { return arr.Length; } }
+        
+
+
+        public MyList()
         {
             arr = new T[0];
+           
             
         }
+
+        public MyList( int arrlenght)
+        {
+            arr = new T[arrlenght];
+        }
+
         public  void Add(T a)
         {
             T[] temparr = new T[arr.Length +1];
@@ -31,5 +45,51 @@ namespace Domain.Models.ListModels
             }
 
         }
+
+        public T this[int productindex]
+        {
+            get { return arr[productindex]; }
+            set { arr[productindex] = value; }
+        }
+
+        public T Find (Predicate<T> function)
+        {
+            foreach (var item in arr)
+            {
+                if (function(item))
+                {
+                    return item;
+                }              
+            }
+            return default(T);
+        }
+        public void Clear()
+        {
+            arr = new T[0];
+        }
+
+        public void AddRange(IEnumerable<T> m)
+        {
+            T[] temp = new T[arr.Length + m.Count()];
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (i< arr.Length)
+                {
+                    temp[i] = arr[i];
+                }
+                else
+                {
+                    temp[i] = m.ElementAt(i - arr.Length);
+                }
+            }
+        }
+
+
+        public IEnumerator GetEnumerator()
+        {
+            return arr.GetEnumerator();
+        }
+
+      
     }
 }
--- Week14Homework/Program.cs
+++ Week14Homework/Program.cs
@@ -12,12 +12,24 @@ LoginController.CheckLoginInput();
 FactorialController.CheckFactorial();
 #endregion
 
-#region Add To Generic List
+#region Cutom List Methods 
 MyList<int> list = new MyList<int>();
+list.Add(3);
 list.Add(7);
+list.Add(2);
+list.Add(1);
 list.Add(7);
-list.Add(7);
-list.Add(7);
-list.Add(7);
+
+MyList<int> list2 = new MyList<int>();
+list2.Add(3);
+list2.Add(7);
+list2.Add(2);
+list2.Add(1);
+list2.Add(7);
+
+Console.WriteLine(list.Find(a => a < 2));
+Console.WriteLine(list.FindAll(a => a >5));
+list.Clear();
+
 #endregion
 
